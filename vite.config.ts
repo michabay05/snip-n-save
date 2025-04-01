58b3abe08b99bf1acb6dd142ed08fa2364d557bd
michabay05 <michabay05@gmail.com>
2025-03-31 21:12:10 -0400

    Fixed issue with not finding correct font files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-    ]
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "public/manifest.json",
+                    dest: ".",
+                },
+            ],
+        }),
+    ],
+    build: {
+        outDir: "dist",
+        rollupOptions: {
+            input: "./index.html"
+        }
+    }
 });
